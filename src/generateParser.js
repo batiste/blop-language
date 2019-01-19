0982e9b50e96f3bec589b45d397a689e72659421
Batiste Bieler <batiste.bieler@gmail.com>
2019-01-19 18:03:22 +0100

    Write a couple more test

--- src/generateParser.js
+++ src/generateParser.js
@@ -5,9 +5,7 @@ const path = require('path');
 const { performance, PerformanceObserver } = require('perf_hooks');
 const { grammar } = require('./grammar');
 const { tokensDefinition } = require('./tokensDefinition');
-const backend = require('./backend');
 const meta = require('./metaParserGenerator');
-const utils = require('./utils');
 
 const obs = new PerformanceObserver((items) => {
   const measurement = items.getEntries()[0];
@@ -26,34 +24,3 @@ fs.writeFileSync(path.resolve(__dirname, './parser.js'),
 
 performance.mark('B');
 performance.measure('Writting parser code', 'A', 'B');
-
-const { code } = require('./codeExample');
-
-const lines = code.split(/\r\n|\r|\n/).length;
-console.log(`Testing parser: parsing ${lines} lines of generated blop code.`);
-
-const out = require('./parser');
-
-performance.mark('C');
-
-const stream = out.tokenize(tokensDefinition, code);
-
-performance.mark('D');
-performance.measure('Tokenization', 'C', 'D');
-
-
-performance.mark('E');
-const tree = out.parse(stream, 0);
-
-if (!tree.success) {
-  utils.displayError(code, stream, tokensDefinition, grammar, tree);
-}
-performance.mark('K');
-performance.measure('Parsing', 'E', 'K');
-
-performance.mark('F');
-backend.generateCode(tree, stream, code).join('');
-performance.mark('G');
-performance.measure('Code generation', 'F', 'G');
-
-performance.measure('Total', 'A', 'G');
