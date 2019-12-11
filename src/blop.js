21c594e8c10937d3908c98cde9e7f68f62d8cca1
Batiste Bieler <batiste.bieler@gmail.com>
2019-12-11 18:03:13 +0100

    Add a perf test

--- src/blop.js
+++ src/blop.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const fs = require('fs');
+const vm = require('vm');
 const { compileSource } = require('./compile');
 
 const GREEN = '\x1b[32m';
@@ -11,6 +12,7 @@ program
   .version('0.1.0')
   .option('-i, --input <file>', 'file input')
   .option('-o, --output <file>', 'file output')
+  .option('-e, --execute', 'execute the input')
   .option('-r, --resolve', 'resolve import statements')
   .option('-s, --sourceMap', 'add source maps')
   .parse(process.argv);
@@ -19,22 +21,35 @@ if (!process.argv.slice(2).length || !program.input) {
   program.outputHelp();
 }
 
-if (program.input) {
-  const source = fs.readFileSync(program.input);
-  const result = compileSource(source.toString(), 'node', program.input, program.sourceMap, program.resolve);
+function execute() {
+  if (program.input) {
+    const source = fs.readFileSync(program.input);
+    const result = compileSource(source.toString(), 'node', program.input, program.sourceMap, program.resolve);
 
-  if (program.sourceMap) {
-    const map = Buffer.from(JSON.stringify(result.sourceMap)).toString('base64');
-    const prefix = '//# sourceMappingURL=data:application/json;charset=utf8;base64,';
-    const inlineSourceMap = prefix + map;
-    result.code += inlineSourceMap;
-  }
-  if (program.output) {
-    fs.writeFile(program.output, result.code, (err) => {
-      if (err) throw err;
-      console.log(`${GREEN}[blop]${NC} ${program.output} has been written`);
-    });
-  } else {
-    console.log(result.code);
+    if (program.sourceMap) {
+      const map = Buffer.from(JSON.stringify(result.sourceMap)).toString('base64');
+      const prefix = '//# sourceMappingURL=data:application/json;charset=utf8;base64,';
+      const inlineSourceMap = prefix + map;
+      result.code += inlineSourceMap;
+    }
+
+    if (program.execute) {
+      const script = new vm.Script(result.code);
+      const sandbox = { require, module, console };
+      vm.createContext(sandbox);
+      script.runInContext(sandbox);
+      return;
+    }
+
+    if (program.output) {
+      fs.writeFile(program.output, result.code, (err) => {
+        if (err) throw err;
+        console.log(`${GREEN}[blop]${NC} ${program.output} has been written`);
+      });
+    } else {
+      console.log(result.code);
+    }
   }
 }
+
+execute();
