e36bec21e01e8855c522e7a3c3365d0005547d6f
Batiste Bieler <batiste.bieler@gmail.com>
2020-05-02 19:10:29 +0200

    Try to fix the tests

--- blop.config.js
+++ blop.config.js
@@ -4,6 +4,9 @@ module.exports = {
     'window': true,
     'module': true,
     'AbortController': true,
+    'jest': true,
+    'beforeEach': true,
+    'afterEach': true,
   },
   strictness: 'perfect', // will refuse to compile with a warning
   inference: true,
